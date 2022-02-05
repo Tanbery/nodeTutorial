71d349b72f6a5f1a0200e66a653152741ee491c2
Tanbery <envergul23@yahoo.com>
2022-02-05 19:54:01 +0300

    git deneme

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express   = require("express");
 const app       = express();
 const mng       = require("mongoose");
 
+
 require("dotenv/config");
 const postRouter = require("./routes/posts")
 const bodyParser = require("body-parser");
